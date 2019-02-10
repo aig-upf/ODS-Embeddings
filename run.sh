407102ddc4620a7ae7adfbb7a12ee108aafb8478
Nur Álvarez González <nurloco@hotmail.com>
2019-02-10 19:40:39 +0100

    Safe dir creation

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
 # Prepare sampled directory
-mkdir graph/sampled
+if [[ ! -d "graph/sampled" ]]; then
+    mkdir graph/sampled
+fi
 ./graph/sample.sh
 
 # Create experiments log directory
-mkdir experiments
+if [[ ! -d "experiments" ]]; then
+    mkdir experiments
+fi
+
+# Encode the graphs
 sbatch bin/encodeAndWalkGraph.sh '.' 'experiments' 'bin/train.sh' 'graph/sampled' 'Facebook-P50' '.edgelist' '8'
 sbatch bin/encodeAndWalkGraph.sh '.' 'experiments' 'bin/train.sh' 'graph/sampled' 'CA-AstroPh-P50' '.edgelist' '8'
 
