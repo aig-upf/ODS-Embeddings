4c810350a1249055678acb6a9a7f0b5d818bfd67
Nurudín Álvarez <nalvarez@ntent.com>
2019-04-01 11:49:18 +0200

    Link prediction experiments set-up

--- bin/runLPExperiments.sh
+++ bin/runLPExperiments.sh
@@ -1,61 +1,42 @@
 #!/bin/bash
-#SBATCH -J LinkPredictionExperiments
+#SBATCH -J LinkPredictionGraphs
 #SBATCH -p high
-#SBATCH -n 8 #number of tasks
-#SBATCH -c 8
-#SBATCH --array=1-144:1
+#SBATCH -n 1 #number of tasks
+#SBATCH -c 1
+#SBATCH --mem=8192
+#SBATCH --array=1-11:1
 
 module load python-igraph/0.7.1.post6-foss-2017a-Python-3.6.4
 
-# Load the desired variables
-TARGET_DIR=${1:-.}
-OUTPUT_DIR=${2:-.}
-TRAIN_COMMAND=${3:-bin/train.sh}
-LINK_PREDICTION_COMMAND=${4:-bin/linkPredictionExperiment.sh}
-GRAPH_PATH=${5:-graphs/}
-GRAPH_NAME=${6:-Facebook}
-GRAPH_FORMAT=${7:-.edgelist}
-NUM_THREADS=${8:-8}
-FORCE=${9:-}
+OUTPUT_DIR=${1:-experiments/lp}
+LINK_COMMAND=${2:-bin/linkPredictionExperiment.sh}
+GRAPH_PATH=${3:-graph/sampled}
+GRAPH=${4:-Facebook-1}
 
-# Prepare the whole experiments array, so that we can easily distribute work
-OUTPUTS_ARRAY=()
+D=32; E=250; C=6; M=2; K=2
+KEYS=(D D D E E C C C C M K)
+VALUES=(32 64 128 50 500 2 4 8 10 1 1)
 COMMANDS_ARRAY=()
-for D in 32 64 128;
-do
-  for E in 50 250 500;
-  do
-    for C in 2 4 6 8; # 2 3 4 5;
-    do
-      for M in 1 2; # 1 2 3;
-      do
-        for K in 1 2; # 1 2 3;
-        do
-          TARGET_ENC="$GRAPH_NAME-K$K.json"
-          TARGET_WALK="$GRAPH_NAME-K$K.walk"
-          TARGET_EMB="$GRAPH_NAME-K$K-D$D-E$E-C$C-M$M.emb"
-          OUTPUT_FILE="$OUTPUT_DIR/$GRAPH_NAME-K$K-D$D-E$E-C$C-M$M.log"
-          DELETE_PATH="rm -f '$TARGET_DIR/emb/$TARGET_EMB'"
-          CMD="$TRAIN_COMMAND '$GRAPH_PATH/$GRAPH_NAME$GRAPH_FORMAT' '$TARGET_DIR/labels/$TARGET_ENC' '$TARGET_DIR/walk/$TARGET_WALK' '$TARGET_DIR/emb/$TARGET_EMB' '-d $K' '' '-d $D -c $C -e $E -M $M' '-t $NUM_THREADS -v 2'; $LINK_PREDICTION_COMMAND '$TARGET_DIR/emb/$TARGET_EMB' '$GRAPH_PATH$GRAPH_NAME-C$GRAPH_FORMAT' '$TARGET_DIR/labels/$TARGET_ENC'";
-          if [[ ! -z "$FORCE" ]]; then
-            CMD="$DELETE_PATH; $CMD"
-          fi
-          COMMANDS_ARRAY+=("$CMD")
-          OUTPUTS_ARRAY+=("$OUTPUT_FILE")
-        done
-      done
-    done
-  done
+for i in `seq 0 $((${#KEYS[@]} - 1))`; do
+  SETTING=${KEYS[$i]}
+  VALUE=${VALUES[$i]}
+  _D=$D; _E=$E; _C=$C; _M=$M; _K=$K;
+  declare _$SETTING=$VALUE
+
+  INPUT_PATH="$GRAPH_PATH/$GRAPH-C.edgelist"
+  ENCODING_PATH="labels/$GRAPH-K$_K.json"
+  EMB_PATH="emb/$GRAPH-K$_K-D$_D-E$_E-C$_C-M$_M.emb"
+  CMD="$TRAIN_COMMAND '$EMB_PATH' '$INPUT_PATH' '$ENCODING_PATH'";
+  OUTPUT="$OUTPUT_DIR/$GRAPH-K$_K-D$_D-E$_E-C$_C-M$_M.log"
+  COMMANDS_ARRAY+=("$CMD")
+  OUTPUTS_ARRAY+=("$OUTPUT")
 done
 
-# Run the array of jobs, dumping the output to file
-# - If the Slurm environment variable is active, only one job will run
-# - Otherwise, it will run all the tasks sequentially
 if [[ ! -z "$SLURM_ARRAY_TASK_ID" ]]; then
-  INDEX=$((SLURM_ARRAY_TASK_ID-1))
-  eval "${COMMANDS_ARRAY[$INDEX]}" > ${OUTPUTS_ARRAY[$INDEX]}
+  INDEX=$((SLURM_ARRAY_TASK_ID - 1))
+  eval "${COMMANDS_ARRAY[$INDEX]} >> ${OUTPUTS_ARRAY[$INDEX]}" 
 else
-  for i in `seq ${#COMMANDS_ARRAY[@]}`; do
-    eval "${COMMANDS_ARRAY[$i]}" > ${OUTPUTS_ARRAY[$i]}
+  for i in `seq 0 $((${#COMMANDS_ARRAY[@]} - 1))`; do
+    eval "${COMMANDS_ARRAY[$i]} >> ${OUTPUTS_ARRAY[$i]}"
   done
 fi
