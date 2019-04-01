fa911506e823244ca308eb7c027fff69545c2483
Nurudín Álvarez <nalvarez@ntent.com>
2019-04-01 18:16:24 +0200

    Results in the repo to keep track of what we compute!

--- bin/linkPredictionExperiment.sh
+++ bin/linkPredictionExperiment.sh
@@ -10,7 +10,7 @@ NUM_EXPERIMENTS="${4:-25}"
 # execute necessary steps -- no target output means the step will be ignored
 RESULT="[LINK] $MODEL $GRAPH $LABELS $NUM_EXPERIMENTS"
 for N in `seq $NUM_EXPERIMENTS`; do
-  OUTPUT=$($MAIN -M '$MODEL' link -g '$GRAPH' -m '$LABELS' | grep 'AUC' | sed 's/\"/ /g' | rev | cut -d' ' -f2 | rev)
+  OUTPUT=$($MAIN -M "$MODEL" link -g "$GRAPH" -m "$LABELS" | grep 'AUC' | sed 's/\"/ /g' | rev | cut -d' ' -f2 | rev)
   RESULT="$RESULT $OUTPUT"
 done
 echo "$RESULT"
